5369d933683a968aa815dedf5c9bf3c27810ca92
Myshawne <Myshawnestallings@gmail.com>
2019-10-16 13:39:38 -0700

    added addition method with passing parameters

--- myfirst_method.rb
+++ myfirst_method.rb
@@ -1,1 +1,13 @@
-def
+# def my_first_method
+#     puts "This is my first method!"
+# end
+
+# my_first_method
+
+def math_method(num1, num2)
+    puts num1 + num2
+end
+
+math_method(2,3)
+math_method(200,123456789)
+math_method(99999,202393)
